45d08dd6eb290b3ee0b4882bfed9e9181dd3ce3e
lzj <zhongjie.li@viziner.cn>
2016-12-20 16:14:15 +0800

    finish

--- requests_seesion_demo.py
+++ requests_seesion_demo.py
@@ -6,7 +6,7 @@
 # Author: zhongjie.li
 # email: zhongjie.li@viziner.cn
 # Created Time: 2016-12-19 09:57:24
-# Last Modified: 2016-12-20 13:52:47
+# Last Modified: 2016-12-20 16:13:53
 ############################
 
 import requests
@@ -20,16 +20,24 @@ sys.setdefaultencoding('utf-8')
 
 home_url = "http://www.dowater.com/nijian/"
 base_url = "http://www.dowater.com/"
+date_stamp = time.strftime('%Y-%m-%d')
+path = os.getcwd()
+new_path = os.path.join(path, '拟建项目', date_stamp)
+if not os.path.isdir(new_path):
+    os.makedirs(new_path)
 
 
 class Tool:
+    '''
+    去除多余符号
+    '''
     replaceTD = re.compile('<td>')
     replacePara = re.compile('<P.*?>|</P>')
     replaceBR = re.compile('<BR><BR>|<BR>')
     removeAddr = re.compile('<A.*?>|</A>')
     removeFont = re.compile('<FONT.*?>|</FONT>')
     # removeImg = re.compile('<img.*?>| {7}|')
-    # removeExtraTag = re.compile('<.*?>')
+    removeExtraTag = re.compile('\r|\n')
 
     def replace(self, x):
         x = re.sub(self.replaceTD, " ", x)
@@ -38,11 +46,14 @@ class Tool:
         # x = re.sub(self.removeImg, " ", x)
         x = re.sub(self.removeFont, " ", x)
         x = re.sub(self.removeAddr, "", x)
-        # x = re.sub(self.removeExtraTag, " ", x)
+        x = re.sub(self.removeExtraTag, " ", x)
         return x.strip()
 
 
 class Session_client(object):
+    '''
+    requests 连接类
+    '''
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
@@ -58,6 +69,9 @@ class Session_client(object):
         self._session.headers = self.headers
 
     def login(self, username, password):
+        '''
+        登陆方法，通过返回页面中的用户来确定是否登录成功
+        '''
         self._username = username
         self._password = password
         self._loginurl = self.loginURL
@@ -81,38 +95,58 @@ class Session_client(object):
                 print("登录失败，重新输入")
 
     def open(self, url, delay=0, timeout=10):
+        '''
+        requests 打开url方法
+        '''
         if delay:
             time.sleep(delay)
         return self._session.get(url, timeout=timeout)
 
 
 class Get_info(object):
+    '''
+    抓取信息类
+    '''
 
     def __init__(self, username, password):
+        '''
+        初始化 session实例化，tool实例化
+        '''
         self.c = Session_client()
         self.c.login(username, password)
         self.tool = Tool()
 
     def get_full_url(self, home_url):
+        '''
+        抓取页面所有需要的url
+        '''
         sul = self.c.open(home_url)
-        print(sul.content.decode('gb2312').encode('utf-8'))
+        # print(sul.content.decode('gb2312').encode('utf-8'))
         pattern = re.compile(
-            r'<li class="listlink_nijian">.*?href="([^"]*)".*?</li>', re.S)
+            r'<li class="listlink_nijian">.*?href="(/nijian/(.*?)/[^"]*)".*?</li>', re.S)
         links = re.findall(pattern, sul.content)
         full_links = []
         for i in links:
-            full_links.append(base_url + i)
+            if i[1] == date_stamp:
+                full_links.append(base_url + i[0])
+        # print full_links
         return full_links
 
     def save_mess(self, title, info_dic):
+        '''
+        以json形式保存信息
+        '''
         try:
-            with open('nijian' + '/' + title + '.txt', 'wb') as f:
+            with open(new_path + '/' + title + '.txt', 'wb') as f:
                 jsonObj = json.dumps(info_dic, ensure_ascii=False)
                 f.write(jsonObj)
         except Exception, e:
             print e
 
     def get_mess(self, url_dic):
+        '''
+        抓取有用信息
+        '''
         patterns_3 = re.compile(
             r'<TD class=[^>]*>(.*?)</TD>.*?<TD[^>]*>(.*?)</TD>', re.S)
         for item in url_dic:
@@ -128,7 +162,7 @@ class Get_info(object):
 
 
 if __name__ == "__main__":
-    g = Get_info("artronics", "hayi2017")
-    # g.get_full_url(home_url)
-    url_dic = ["http://www.dowater.com/nijian/2016-12-20/517454.asp"]
-    g.get_mess(url_dic)
+    g = Get_info("artronics", "hayi")
+    dic = g.get_full_url(home_url)
+    # url_dic = ["http://www.dowater.com/nijian/2016-12-20/517454.asp"]
+    g.get_mess(dic)
